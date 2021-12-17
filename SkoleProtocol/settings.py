b46f3678a19dc4e70277a498d8da963df64b4bef
Nadia Hansen <nadiahansen.kea@gmail.com>
2021-12-17 14:49:45 +0100

    change databse

--- SkoleProtocol/settings.py
+++ SkoleProtocol/settings.py
@@ -86,14 +86,11 @@ WSGI_APPLICATION = 'SkoleProtocol.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Skoleprotocol',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'PASSWORD': '1234!',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
